6c3cf4678a1f5bc823f57d4a67c4fb4f3a67e758
Gouthamkumar543 <gouthamkumar0345@gmail.com>
2025-03-31 13:39:27 +0530

    changed

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
       <Routes>
         <Route path='/signup' element={<SignUp />} />
         <Route path='/login' element={<LogIn />} />
-        <Route path='/home' element={<DashBoard/>}/>
+        <Route path='/' element={<DashBoard/>}/>
       </Routes>
     </div>
   );
